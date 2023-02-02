73e903c5083f733536b7b3c1605da318ea09d0c8
Almir <a1mir@mail.ru>
2023-02-02 19:28:52 +0600

    added file lists

---
+++ TSIS 2/2lists.py
@@ -0,0 +1,24 @@
+#example1
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
+
+#example2
+thislist = ["apple", "banana", "cherry"]
+print(len(thislist))
+
+#example3
+list1 = ["apple", "banana", "cherry"]
+list2 = [1, 5, 7, 9, 3]
+list3 = [True, False, False]    
+
+#example4
+list1 = ["abc", 34, True, 40, "male"]
+
+#example5
+mylist = ["apple", "banana", "cherry"]
+print(type(mylist))
+
+#example6
+thislist = list(("apple", "banana", "cherry")) # note the double round-brackets
+print(thislist)
+
