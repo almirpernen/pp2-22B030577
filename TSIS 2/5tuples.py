7d98e9875bf5c336670185e7b12c1fcdd358d004
Almir <a1mir@mail.ru>
2023-02-02 19:46:05 +0600

    added file for tuples

---
+++ TSIS 2/5tuples.py
@@ -0,0 +1,37 @@
+#exercise1
+thistuple = ("apple", "banana", "cherry")
+print(thistuple)
+
+#exercise2 
+thistuple = ("apple", "banana", "cherry", "apple", "cherry")
+print(thistuple)
+
+#exercise3
+thistuple = ("apple", "banana", "cherry")
+print(len(thistuple))
+
+#exercise4
+thistuple = ("apple",)
+print(type(thistuple))
+
+#NOT a tuple
+thistuple = ("apple")
+print(type(thistuple))
+
+
+#exercise5
+tuple1 = ("apple", "banana", "cherry")
+tuple2 = (1, 5, 7, 9, 3)
+tuple3 = (True, False, False)
+
+#exercise6
+tuple1 = ("abc", 34, True, 40, "male")
+
+
+#exercise7
+mytuple = ("apple", "banana", "cherry")
+print(type(mytuple))    
+
+#exercise8
+thistuple = tuple(("apple", "banana", "cherry")) # note the double round-brackets
+print(thistuple)
