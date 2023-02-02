c6c4724d2413d9d9d5df74dbf878c580ed6bc08b
Almir <a1mir@mail.ru>
2023-02-02 19:40:19 +0600

    added file for arrays

---
+++ TSIS 2/4arrays.py
@@ -0,0 +1,24 @@
+#example1
+cars = ["Ford", "Volvo", "BMW"]
+
+#example2
+x = cars[0]
+
+#example3
+cars[0] = "Toyota"
+
+#example4
+x = len(cars)
+
+#example5
+for x in cars:
+  print(x)
+
+#example6
+cars.append("Honda")
+
+#example7
+cars.pop(1)
+
+#example8
+cars.remove("Volvo")
