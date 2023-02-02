7b08570e00df1ba2915d06f3ce2440a055d5bf6e
Almir <a1mir@mail.ru>
2023-02-02 19:48:40 +0600

    added file for sets

---
+++ TSIS 2/6sets.py
@@ -0,0 +1,31 @@
+#example1
+thisset = {"apple", "banana", "cherry"}
+print(thisset)
+
+#example2
+thisset = {"apple", "banana", "cherry", "apple"}
+
+print(thisset)
+
+#example3
+thisset = {"apple", "banana", "cherry"}
+
+print(len(thisset))
+
+#example4
+set1 = {"apple", "banana", "cherry"}
+set2 = {1, 5, 7, 9, 3}
+set3 = {True, False, False}
+
+
+#example5
+set1 = {"abc", 34, True, 40, "male"}
+
+
+#example6
+myset = {"apple", "banana", "cherry"}
+print(type(myset))
+
+#example7
+thisset = set(("apple", "banana", "cherry")) # note the double round-brackets
+print(thisset)
