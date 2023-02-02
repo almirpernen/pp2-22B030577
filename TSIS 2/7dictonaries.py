3367c2faeca28cd1ccd0243f7850ffafbb281bdb
Almir <a1mir@mail.ru>
2023-02-02 19:51:33 +0600

    added file for dictonaries

---
+++ TSIS 2/7dictonaries.py
@@ -0,0 +1,48 @@
+#example1
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict)
+
+#example2
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict["brand"])
+
+#example3
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964,
+  "year": 2020
+}
+print(thisdict)
+
+#example4
+print(len(thisdict))
+
+#example5
+thisdict = {
+  "brand": "Ford",
+  "electric": False,
+  "year": 1964,
+  "colors": ["red", "white", "blue"]
+}
+
+#example6
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(type(thisdict))
+
+
+#example7
+thisdict = dict(name = "John", age = 36, country = "Norway")
+print(thisdict)
