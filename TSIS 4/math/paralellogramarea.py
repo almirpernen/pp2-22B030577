dd0227491e02c96780134770d01f9dd8d4717ea4
Almir <a1mir@mail.ru>
2023-02-18 14:22:41 +0600

    added program to find area of paralellogram

---
+++ TSIS 4/math/paralellogramarea.py
@@ -0,0 +1,7 @@
+import math
+
+def parallelogramArea():
+    length = int(input("Length of base: "))
+    height = int(input("Height of parallelogram: "))
+    area = float(length * height)
+    return f"Expected Output: {area}"
