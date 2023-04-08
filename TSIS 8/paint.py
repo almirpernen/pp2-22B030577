a41a357499d73c3ff94064a94b3ae1c9335ec786
Almir <a1mir@mail.ru>
2023-04-08 11:56:24 +0600

    finished Paint

--- TSIS 8/paint.py
+++ TSIS 8/paint.py
@@ -1,156 +1,92 @@
 import pygame
 
-pygame.init()
-screen = pygame.display.set_mode((800, 600))
-clock = pygame.time.Clock()
-screen2 = pygame.Surface((800, 600))
-pygame.display.set_caption('Paint')
+white = (255, 255, 255)
+eraser = (0, 0, 0)
+green = (34, 139, 34)
+blue = (0, 0, 255)
+red = (255, 0, 0)
+yellow = (255, 255, 0)
 
-radius = 5
-x = 0
-y = 0
-points = []
-colors = {"blue": (0, 0, 255), "red": (255, 0, 0), "green": (0, 255, 0), "yellow": (255, 255, 0), "black": (0, 0, 0),
-          "white": (255, 255, 255), "cyan": (0, 255, 255)}
-color = colors["black"]
-mouse_down = False
-screen2.fill((colors["white"]))
-screen.fill((colors["white"]))
-mode = 1
-prevX = -1
-prevY = -1
-currentX = -1
-currentY = -1
 
-while True:
-    pygame.draw.rect(screen, colors["black"], pygame.Rect(10, 10, 30, 30))
-    pygame.draw.rect(screen, colors["red"], pygame.Rect(43, 10, 30, 30))
-    pygame.draw.rect(screen, colors["blue"], pygame.Rect(76, 10, 30, 30))
-    pygame.draw.rect(screen, colors["green"], pygame.Rect(109, 10, 30, 30))
-    pygame.draw.rect(screen, colors["yellow"], pygame.Rect(142, 10, 30, 30))
-    pygame.draw.rect(screen, colors["cyan"], pygame.Rect(175, 10, 30, 30))
-    pygame.draw.rect(screen, colors["black"], pygame.Rect(10, 43, 30, 30), 1)
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((800, 800))
+    pygame.display.set_caption("Paint")
+    clock = pygame.time.Clock()
 
-    mouse_pos = pygame.mouse.get_pos()
-    mouse_button = pygame.mouse.get_pressed()
-    keys = pygame.key.get_pressed()
+    radius = 15
+    mode = white
+    last_pos = None
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
-                pygame.quit()
-            if event.key == pygame.K_BACKSPACE:
-                screen.fill((colors["white"]))
-            if event.key == pygame.K_l:
-                mode = 1
-            if event.key == pygame.K_c:
-                mode = 2
-            if event.key == pygame.K_r:
-                mode = 3
+    while True:
+        # handle events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    return
 
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 1:
-                mouse_down = True
-        if event.type == pygame.MOUSEBUTTONUP:
-            if event.button == 1:
-                screen2.blit(screen, (0, 0))
-                mouse_down = False
+                # determine if a letter key was pressed
+                if event.key == pygame.K_r:
+                    mode = red
+                elif event.key == pygame.K_g:
+                    mode = green
+                elif event.key == pygame.K_b:
+                    mode = blue
+                elif event.key == pygame.K_y:
+                    mode = yellow
+                elif event.key == pygame.K_BACKSPACE:
+                    mode = eraser
+                elif event.key == pygame.K_w:
+                    drawRectangle(screen, pygame.mouse.get_pos(), 200, 100, mode)
+                elif event.key == pygame.K_c:
+                    drawCircle(screen, pygame.mouse.get_pos(), mode)
 
-        if (keys[pygame.K_RCTRL] or keys[pygame.K_LCTRL]) and (
-                event.type == pygame.KEYDOWN and event.key == pygame.K_UP):
-            radius += 2
-        if (keys[pygame.K_RCTRL] or keys[pygame.K_LCTRL]) and (
-                event.type == pygame.KEYDOWN and event.key == pygame.K_DOWN):
-            radius -= 2
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                # start a new line
+                last_pos = pygame.mouse.get_pos()
 
-        if mouse_down == True:
-            if mouse_pos[0] >= 10 and mouse_pos[0] <= 40 and mouse_pos[1] >= 10 and mouse_pos[1] <= 40:
-                color = colors["black"]
-            if mouse_pos[0] >= 43 and mouse_pos[0] <= 73 and mouse_pos[1] >= 10 and mouse_pos[1] <= 40:
-                color = colors["red"]
-            if mouse_pos[0] >= 76 and mouse_pos[0] <= 106 and mouse_pos[1] >= 10 and mouse_pos[1] <= 40:
-                color = colors["blue"]
-            if mouse_pos[0] >= 109 and mouse_pos[0] <= 139 and mouse_pos[1] >= 10 and mouse_pos[1] <= 40:
-                color = colors["green"]
-            if mouse_pos[0] >= 142 and mouse_pos[0] <= 172 and mouse_pos[1] >= 10 and mouse_pos[1] <= 40:
-                color = colors["yellow"]
-            if mouse_pos[0] >= 175 and mouse_pos[0] <= 205 and mouse_pos[1] >= 10 and mouse_pos[1] <= 40:
-                color = colors["cyan"]
-            if mouse_pos[0] >= 10 and mouse_pos[0] <= 40 and mouse_pos[1] >= 33 and mouse_pos[1] <= 73:
-                color = colors["white"]
+            if event.type == pygame.MOUSEMOTION and event.buttons[0]:
+                # draw a line from the last point to the current point
+                if last_pos is not None:
+                    start_pos = last_pos
+                    end_pos = pygame.mouse.get_pos()
+                    drawLineBetween(screen, start_pos, end_pos, radius, mode)
+                    last_pos = end_pos
 
-        if mode == 1:
-            screen.blit(screen2, (0, 0))
-            if mouse_down == True:
-                if event.type == pygame.MOUSEMOTION:
-                    position = event.pos
-                    points = points + [position]
-            else:
-                points = []
+        pygame.display.flip()
+        clock.tick(60)
 
-            i = 0
-            while i < len(points) - 1:
-                dx = points[i][0] - points[i + 1][0]
-                dy = points[i][1] - points[i + 1][1]
-                iterations = max(abs(dx), abs(dy))
 
-                for j in range(iterations):
-                    progress = 1.0 * j / iterations
-                    aprogress = 1 - progress
-                    x = int(aprogress * points[i][0] + progress * points[i + 1][0])
-                    y = int(aprogress * points[i][1] + progress * points[i + 1][1])
-                    pygame.draw.circle(screen, color, (x, y), radius)
-                i += 1
+def drawLineBetween(screen, start, end, width, color_mode):
+    c1 = max(0, min(255, 2 * start[0] - 256))
+    c2 = max(0, min(255, 2 * start[1]))
 
-        if mode == 2:
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if event.button == 1:
-                    mouse_down = True
-                    currentX = event.pos[0]
-                    currentY = event.pos[1]
-                    prevX = event.pos[0]
-                    prevY = event.pos[1]
+    color = color_mode
+    dx = start[0] - end[0]
+    dy = start[1] - end[1]
+    iterations = max(abs(dx), abs(dy))
 
-            if event.type == pygame.MOUSEBUTTONUP:
-                mouse_down = False
-                screen2.blit(screen, (0, 0))
+    for i in range(iterations):
+        progress = 1.0 * i / iterations
+        aprogress = 1 - progress
+        x = int(aprogress * start[0] + progress * end[0])
+        y = int(aprogress * start[1] + progress * end[1])
+        pygame.draw.circle(screen, color, (x, y), width)
 
-            if event.type == pygame.MOUSEMOTION:
-                if mouse_down:
-                    currentX = event.pos[0]
-                    currentY = event.pos[1]
 
-            if mouse_down and prevX != -1 and prevY != -1 and currentX != -1 and currentY != -1:
-                screen.blit(screen2, (0, 0))
-                pygame.draw.circle(screen, color, (prevX, prevY), max(abs(prevX - currentX), abs(prevY - currentY)),
-                                   radius)
+def drawRectangle(screen, mouse_pos, w, h, color):
+    x = mouse_pos[0]
+    y = mouse_pos[1]
+    rect = pygame.Rect(x, y, w, h)
+    pygame.draw.rect(screen, color, rect, 3)  # 4th parameter is outline of the rectangle
 
-        if mode == 3:
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if event.button == 1:
-                    mouse_down = True
-                    currentX = event.pos[0]
-                    currentY = event.pos[1]
-                    prevX = event.pos[0]
-                    prevY = event.pos[1]
 
-            if event.type == pygame.MOUSEBUTTONUP:
-                mouse_down = False
-                screen2.blit(screen, (0, 0))
+def drawCircle(screen, mouse_pos, color):
+    x = mouse_pos[0]
+    y = mouse_pos[1]
+    pygame.draw.circle(screen, color, (x, y), 100, 3)  # 4th parameter is outline of the rectangle
 
-            if event.type == pygame.MOUSEMOTION:
-                if mouse_down:
-                    currentX = event.pos[0]
-                    currentY = event.pos[1]
 
-            if mouse_down and prevX != -1 and prevY != -1 and currentX != -1 and currentY != -1:
-                screen.blit(screen2, (0, 0))
-                pygame.draw.rect(screen, color,
-                                 pygame.Rect(min(prevX, currentX), min(prevY, currentY), abs(prevX - currentX),
-                                             abs(prevY - currentY)), radius)
-
-    pygame.display.flip()
-
-    clock.tick(60)
+main()
